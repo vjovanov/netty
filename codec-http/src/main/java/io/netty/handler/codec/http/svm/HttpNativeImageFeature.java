b28102fc9c50987dc032c883e031472954acbcbb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-01-06 14:11:48 +0100

    [netty-8c9.1.3] Modify HttpNativeImageFeature (runtime + build-time)
    Added NativeImageBuildOptions.shouldApply() guard to HttpNativeImageFeature to gate both runtime AND build-time initialization. When io.netty.avoidBuildTimeInit=true is set, all initialization logic is skipped.

--- codec-http/src/main/java/io/netty/handler/codec/http/svm/HttpNativeImageFeature.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/svm/HttpNativeImageFeature.java
@@ -15,6 +15,7 @@
  */
 package io.netty.handler.codec.http.svm;
 
+import io.netty.util.internal.svm.NativeImageBuildOptions;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
 
@@ -22,6 +23,10 @@ public class HttpNativeImageFeature implements Feature {
 
     @Override
     public void beforeAnalysis(BeforeAnalysisAccess access) {
+        if (!NativeImageBuildOptions.shouldApply()) {
+            return;
+        }
+
         // Build-time initialization
         RuntimeClassInitialization.initializeAtBuildTime(
                 "io.netty"
