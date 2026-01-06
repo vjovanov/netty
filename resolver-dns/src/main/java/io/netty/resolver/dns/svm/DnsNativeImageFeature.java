0fa2f34818d877ef538ec00c74aaf8307e496da0
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-01-06 12:59:26 +0100

    [netty-8oa.9] Migrate netty-resolver-dns Configuration
    Migrate native-image.properties in netty-resolver-dns to programmatic Feature implementation.
    
    - Create DnsNativeImageFeature to handle runtime initialization of DNS classes
    - Register DnsNativeImageFeature as a service
    - Remove migrated native-image.properties
    - Add package-info.java

---
+++ resolver-dns/src/main/java/io/netty/resolver/dns/svm/DnsNativeImageFeature.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.resolver.dns.svm;
+
+import org.graalvm.nativeimage.hosted.Feature;
+import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
+
+public class DnsNativeImageFeature implements Feature {
+    @Override
+    public void beforeAnalysis(BeforeAnalysisAccess access) {
+        RuntimeClassInitialization.initializeAtRunTime(
+                "io.netty.resolver.dns.DefaultDnsServerAddressStreamProvider",
+                "io.netty.resolver.dns.DnsServerAddressStreamProviders$DefaultProviderHolder",
+                "io.netty.resolver.dns.DnsNameResolver",
+                "io.netty.resolver.dns.DnsNameResolverBuilder",
+                "io.netty.resolver.HostsFileEntriesResolver",
+                "io.netty.resolver.dns.ResolvConf$ResolvConfLazy");
+    }
+}
